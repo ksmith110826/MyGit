38e85071aeaf5d0b6646def2475efd7cf22eab95
Ricky Chiu <rchiu0108@gmail.com>
2018-07-15 14:06:14 -0700

    Initial Commit

---
+++ MyGit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyGit
+//
+//  Created by Ricky Chiu on 7/15/18.
+//  Copyright © 2018 Ricky Chiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
